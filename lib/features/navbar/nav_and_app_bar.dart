b6e46db526d48617919af48dedb2d5265cf966ee
abdullah-37 <abdullahshoukat031@gmail.com>
2026-05-23 12:53:29 +0500

    push to github

---
+++ lib/features/navbar/nav_and_app_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NavAndAppBar extends StatefulWidget {
+  const NavAndAppBar({super.key});
+
+  @override
+  State<NavAndAppBar> createState() => _NavAndAppBarState();
+}
+
+class _NavAndAppBarState extends State<NavAndAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
